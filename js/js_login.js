1aa6e0b8e953c61b289fb76f5e650f131e4d0720
Eko Sigit <baitosabtu@gmail.com>
2013-02-05 10:03:36 +0700

    halaman muka
    css
    js

---
+++ js/js_login.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#frm_login').submit(function(){
+        $.post('tes.php',$('#frm_login').serialize(),function(data){
+            $('#error').html(data).fadeIn(500).delay(5000).fadeOut(500);
+        });
+        return false;
+    });
+});
