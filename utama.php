2b15cfcce29331633cddf11752fce9931bf81e65
Eko Sigit <baitosabtu@gmail.com>
2013-02-08 02:21:33 +0700

    Desain Hal Utama Raw

--- utama.php
+++ utama.php
@@ -1,18 +1,50 @@
-html>
+<html>
 <head>
     <title>SI-REKAN</title>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/main_style.css" />
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/xxx.js"></script>
 </head>
 <body>
-	<div id="utama">
-		
-	</div>	
-	<div id="footer">
-        Copyright 2013 by Eko Sigit Purnomo
-	</div>
-</div> <!-- end wrapper -->
+	<div id="wrapper">
+		<div id="atas">
+			<div id="logo">
+				<img src="img/depkeu_Logo.png" class="img-rounded" />
+			</div>
+			<div id="header">
+				<div id="header_atas">
+					Sistem Informasi Rekonsiliasi Keuangan Negara<br />
+					(SIREKAN)<br />
+				</div>
+				<div id="header_bawah">
+					<div id="kppn">
+						Kantor Pelayanan Perbendaharaan Negara XXXXXXXXX X
+					</div>
+					<div id="kop">
+						<div id="alamat">
+							Jln. Ngepot Kanan<br />
+							Kab Manasaja, Propinsi Jowo 778787
+						</div>
+						<div id="kontak">
+							No telp : 0312-656566<br />
+							Email : sasas@aasa.com
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="bawah">
+			<div id="kiri">
+				kiri
+			</div>
+			<div id="kanan">
+				kanan
+			</div>
+		</div>	
+		<div id="footer">
+	        Copyright 2013 by Eko Sigit Purnomo
+		</div>
+	</div> <!-- end wrapper -->
 </body>
 </html>
