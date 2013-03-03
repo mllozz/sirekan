0238504e99d59cc5db2a936a2cfee7c1d795b332
Eko Sigit <baitosabtu@gmail.com>
2013-03-03 16:08:46 +0700

    multiple database connection class

--- class/class.Database.php
+++ class/class.Database.php
@@ -2,52 +2,143 @@
 
 class Database {
 
-	private $_host='localhost';
-	private $_user='root';
-	private $_pass='';
-	private $_db='sirekan';
+	//konstan variabel
+	const DB_HOST = 'localhost';
+	const DB_USER = 'root';
+	const DB_PASS = 'root';
+	const DB_SIREKAN = 'sirekan';
+	const DB_VERA = 'sqldb12';
+	const DB_PORT_VERA = '3306';
 	
-	private $_connection;
+	
+	//protected variabel
+	protected $_connection_sirekan;
+	protected $_connection_vera;
+	protected $mysqli_exception;
+	protected $host;
+	protected $user;
+	protected $port;
+	protected $password;
+	protected $db_name;
+	
+	/**
+	*protected konfigurasi
+	*@var array
+	*/
+	protected $config_sirekan;
+	protected $config_vera;
 
 	//simpan single instance
 	private static $_instance;
+	protected static $log;
 
 	/**
-	* Get instance dari database
+	* Get singleton instance dari database
 	* @return type database
 	*/
-	public static function getInstance($index=null) {
-		if($index!=null && $index=='sp2d') {
-			$_db='sqldb12';
-		}
-
+	public static function getInstance() {
 		if(!self::$_instance) {
 			self::$_instance = new self();
 		}
 		return self::$_instance;
 	}
+	
+	
+	/**
+	* Constructor
+	*/
+	protected function __construct() {}
 
-	public function __construct() {
-
-		$this->_connection = new mysqli($this->_host,$this->_user,$this->_pass,$this->_db);
-
-		//error handling
-		if(mysqli_connect_error()) {
-			trigger_error('Gagal terkoneksi ke database : '. mysqli_connect_error(), E_USER_ERROR);
+	/**
+	* Konfigurasi sirekan
+	* @return config array
+	*/
+	protected function configSirekan() {
+		if (isset($this->_connection_sirekan)) {
+			error_log('DATABASE CONNECTION::warning, koneksi ke database sirekan sudah dibuat');
 		}
+
+		$this->config_sirekan = array(
+			'host' => self::DB_HOST,
+			'user' => self::DB_USER,
+			'password' => self::DB_PASS,
+			'db_name' => self::DB_SIREKAN
+		);
 	}
+	
+	/**
+	* Konfigurasi db vera
+	* @return config array
+	*/
+	protected function configVera() {
+		if (isset($this->_connection_vera)) {
+			error_log('DATABASE CONNECTION::warning, koneksi ke database sp2d sudah dibuat');
+		}
+
+		$this->config_vera = array(
+			'host' => self::DB_HOST,
+			'user' => self::DB_USER,
+			'port' => self::DB_PORT_VERA,
+			'password' => self::DB_PASS,
+			'db_name' => self::DB_VERA
+		);
+	}
+	
+	/**
+	* Membuat koneksi mysqli
+	* @return mysqli object
+	*/	
+	protected function createConnection($config=array()){
+		
+		if(is_array($config)) {
+			error_log('DATABASE CONNECTION::warning, konfigurasi koneksi database tidak ada ');
+		}
+		
+		if(count($config)>0) {
+			foreach($config as $name => $value) {
+				$this->$name=$value;
+			}
+		}
+		
+		$new_connection=new mysqli($this->host,$this->user,$this->password,$this->db_name);
+		
+		if(mysqli_connect_error()) {
+			error_log('DATABASE CONNECTION:warning, gagal terkoneksi dengan database');
+			return false;
+		}
+		
+		return $new_connection;
+	}
+
 
 	/**
 	* Empty clone, magic to prevent duplication
 	*/
-
 	private function __clone() {}
 
 	/**
-	* Get mysqli connection
+	* Get Mysqli connection
+	* @param int
+	* @return mysqli Connection
 	*/
-	public function getConnection()
+	public function getConnection($sel=null)
 	{
-		return $this->_connection;
+		if(isset($sel)) {
+			if($sel==1){
+				$this->configSirekan();
+				$this->_connection_sirekan=$this->createConnection($this->config_sirekan);
+				return $this->_connection_sirekan;
+			} else if($sel==2) {
+				$this->configVera();
+				$this->_connection_vera=$this->createConnection($this->config_vera);
+				return $this->_connection_vera;
+			}
+			else {
+				error_log('DATABASE CONNECTION::warning, database selection failed');
+			}
+		}
+		else {
+			error_log('DATABASE CONNECTION::warning, database selection failed');
+		}
 	}
 }
--- tes.php
+++ tes.php
@@ -1,29 +1,37 @@
 <?php
 	function __autoload($class_name) {
-  		include 'class/class.' . $class_name . '.php';
+  		include 'class/class.' . strtolower($class_name) . '.php';
 	}
 
-	$data = array(
-		'kode_ba' =>'015' ,
-		'kode_es' => '08',
-		'kode_satker' => '635172',
-		'nama_satker' => 'KPPN Mana Saaja',
-	);
+	//$data = array(
+	//	'kode_ba' =>'015' ,
+	//	'kode_es' => '08',
+	//	'kode_satker' => '635172',
+	//	'nama_satker' => 'KPPN Mana Saaja',
+	//);
 
-	$satker = new Satker($data);
+	//$satker = new Satker($data);
 
 	//echo $satker;
-
-	$db= Database::getInstance('sp2d');
-	$mysql=$db->getConnection();
-
-	$query='select * from d_sispen';
-
-	$result=$mysql->query($query) or trigger_error(mysqli_connect_error());
-
-	if($result==false) {
-		echo 'error ';
+	$bag=$_GET['data'];
+	$db= Database::getInstance();
+	$mysql=$db->getConnection($bag);
+	
+	if($bag==2) {
+		$query='select * from t_kppn';
+	} else {
+		$query='select * from users';
 	}
 
-	print_r($result);
+	$result=$mysql->query($query) or trigger_error(mysqli_connect_error());
+	
+	if($result==false) {
+		echo 'ra iso';
+	}
+	$data=$result->fetch_assoc();
+
+	print_r($data);
+
+
+	
 ?>
