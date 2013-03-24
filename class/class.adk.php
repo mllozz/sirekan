c9c8813d01483c434b5bda24b9929ce6f826e7ef
Eko Sigit <baitosabtu@gmail.com>
2013-03-24 22:50:13 +0700

    read file done

--- class/class.adk.php
+++ class/class.adk.php
@@ -10,19 +10,48 @@ class Adk {
      */
     public function getFile($filename,$id_rekon) {
         if($id_rekon=='1') {
-            return $this->readSA($filename);
+            //read saldo awal
+            return $this->read($filename);
         }elseif($id_rekon=='2') {
             //read sai
+            return $this->read($filename);
         }else {
             return 'Gagal Mendapatkan file';
         }
     }
     
-    public function readSA($filename){
+    public function read($filename){
         $file='adk/'.$filename;
-        $pointer=fopen($file, 'rb');
-        $data=  fread($pointer, filesize($file));
+        $reader=new Prodigy_DBF($file);
+        $i=0;
+        $data[]=array();
+        while(($hasil=$reader->GetNextRecord(true)) && !empty($hasil)) {
+            $data[$i]=$hasil;
+            $i++;
+        }
         return $data;
+        
+    }
+    
+    /**
+     * Cek apakah pernah rekon saldo awal sebelumnya
+     * @param type $kdbaes
+     * @param type $kdsatker
+     * @return boolean
+     */
+    public function cekRekonSA($kdbaes,$kdsatker) {
+        $thnang=2011;
+        $db=  Database::getInstance();
+        $conn=$db->getConnection(1);
+        
+        $query="SELECT * FROM glsa WHERE KDBAES1='$kdbaes' AND KDSATKER='$kdsatker' AND THNANG='$thnang'";
+        $result=$conn->prepare($query);
+        $result->execute();
+        
+        if($result->rowCount()>=1) {
+            return true;
+        }
+        return false;
     }
 }
 ?>
--- tes.php
+++ tes.php
@@ -1,40 +1,34 @@
 <?php
-	function __autoload($class_name) {
-  		include 'class/class.' . strtolower($class_name) . '.php';
-	}
 
-	//$data = array(
-	//	'kode_ba' =>'015' ,
-	//	'kode_es' => '08',
-	//	'kode_satker' => '635172',
-	//	'nama_satker' => 'KPPN Mana Saaja',
-	//);
+function __autoload($class_name) {
+    include 'class/class.' . strtolower($class_name) . '.php';
+}
 
-	//$satker = new Satker($data);
-
-	//echo $satker;
-	//$bag=$_GET['data'];
-	//$db= Database::getInstance();
-	//$mysql=$db->getConnection($bag);
-	
-	//if($bag==2) {
-	//	$query='select * from t_kppn';
-	//} else {
-	//	$query='select * from users';
-	//}
-
-	//$result=$mysql->query($query) or trigger_error(mysqli_connect_error());
-	//$result=$mysql->prepare($query);
-	//$result->execute();
-	
-	//if($result==false) {
-	//	echo 'ra iso';
-	//}
-	//$data=$result->fetch_assoc();
-	//$data=$result->fetch();
-
+//$data = array(
+//	'kode_ba' =>'015' ,
+//	'kode_es' => '08',
+//	'kode_satker' => '635172',
+//	'nama_satker' => 'KPPN Mana Saaja',
+//);
+//$satker = new Satker($data);
+//echo $satker;
+//$bag=$_GET['data'];
+//$db= Database::getInstance();
+//$mysql=$db->getConnection($bag);
+//if($bag==2) {
+//	$query='select * from t_kppn';
+//} else {
+//	$query='select * from users';
+//}
+//$result=$mysql->query($query) or trigger_error(mysqli_connect_error());
+//$result=$mysql->prepare($query);
+//$result->execute();
+//if($result==false) {
+//	echo 'ra iso';
+//}
+//$data=$result->fetch_assoc();
+//$data=$result->fetch();
 //	print_r($data);
-	
 //	echo '<br />';
 //	echo '<br />';
 //	echo '<br />';
@@ -50,7 +44,6 @@
 //	echo $data->nmkppn;
 //        
 //        echo date('Y-m-d H:i:s');
-	
 //$input = 'bl';
 //$data = array('orange', 'blue', 'green', 'red', 'pink', 'brown', 'black');
 //        
@@ -62,13 +55,13 @@
 //});
 //
 //var_dump($result);
-        
-        $adk=new Adk();
-                        $content=$adk->getFile('SAKD01508635162.012', 1);
-                        print_r($content);
-                        echo '<hr />';
-                        $content=  explode(' ', $content);
-                        print_r($content[0]);
-                        echo '<hr />';
-                        print_r($content[count($content)-1]);
+
+$adk = new Adk();
+$cek=$adk->cekRekonSA('01508', '635162');
+if($cek) {
+    print_r('Sudah rekon');
+} else {
+    $content = $adk->getFile('SAKD01508635162.012', 1);
+    print_r($content[0]['KDKANWIL']);
+}
 ?>
